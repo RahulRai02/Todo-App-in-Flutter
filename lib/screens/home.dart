f84536f84ba42bb60d0188aa68f95b91c665009f
Rahul Rai <rahulrai@Rahuls-MacBook-Air.local>
2022-12-30 02:59:23 +0530

    Step3: Bottom bar done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,39 +36,96 @@ class Home extends StatelessWidget {
               )
             ],
           )),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 50,
-                      bottom: 20,
-                    ),
-                    child: Text(
-                      'All ToDos',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 15,
+            ),
+            // color: Colors.grey,
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
+                      for (Todo todo in todoListStat)
+                        ToDoItem(
+                          note: todo,
+                        ),
+
+                      // ToDoItem(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add new ToDo item',
+                      border: InputBorder.none,
                     ),
                   ),
-                  for (Todo todo in todoListStat)
-                    ToDoItem(
-                      note: todo,
-                    ),
-
-                  // ToDoItem(),
-                ],
+                ),
               ),
-            ),
-          ],
-        ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20, right: 20),
+                // ignore: sort_child_properties_last
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 45),
+                  
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    minimumSize: Size(50, 50),
+                    elevation: 10,
+                  
+                  ),
+                ),
+
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                ),
+              )
+            ]),
+          )
+        ],
       ),
     );
   }
